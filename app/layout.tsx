8b6daa1c8d03a738b79f2a5baa6ebccc5970fc82
Alves Mickaël <alves.mckl@gmail.com>
2023-03-18 17:34:18 +0100

    🌗Adding theme switcher (#21)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import '../styles/reset.scss';
 import React from "react";
 import Header from "../components/header";
 import {Rubik} from "@next/font/google";
+import {ThemeProvider} from "next-themes";
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -17,8 +18,10 @@ export default function RootLayout({children}: RootLayoutProps) {
   return (
     <html lang="fr" className={rubik.className}>
     <body>
+    <ThemeProvider>
       <Header/>
       {children}
+    </ThemeProvider>
     </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ export default function Home() {
   return (
     <>
       <div className={styled.home}>
-        <div className={styled.informationsLayer}>
           <div className={styled.details}>
             <h2>Mickaël Alves</h2>
             <h1>Web Developer</h1>
           </div>
-        </div>
       </div>
     </>
   );
