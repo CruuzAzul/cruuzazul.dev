0e914f7f03a88a464db5762ee232092ac828ed76
CruuzAzul <alves.mckl@gmail.com>
2023-03-18 17:33:21 +0100

    🌗Adding theme switcher

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import LogoLight from "../public/images/logo.png";
 import LogoDark from "../public/images/logoDark.png";
 import styled from '../styles/common/header.module.scss';
 import {socials} from "../config/socials";
 import Link from "next/link";
+import ThemeSwitcher from "./themeSwitcher";
+import {useTheme} from "next-themes";
 
 
 export default function Header() {
+  const {theme} = useTheme()
 
   return (
     <header className={styled.header}>
       <nav>
         <div className={styled.logo}>
-          <Image src={LogoDark} alt="logo" height={30}/>
+          <Image src={theme === "dark" ? LogoDark : LogoLight} alt="logo" height={30}/>
         </div>
         <div className={styled.socials}>
           <ul>
@@ -26,6 +30,7 @@ export default function Header() {
               ))
             }
           </ul>
+        <ThemeSwitcher />
         </div>
       </nav>
     </header>
