eb2a4bac95f6ae24b5a7eadf80e314079d6ea3b3
CruuzAzul <alves.mckl@gmail.com>
2023-09-04 21:37:40 +0200

    🤯 Adding meta head

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import {Home} from '../modules/home/Home';
 import {Hero} from '../modules/home/Hero';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Mickaël Alves',
+  description:
+    "I'm truly passionate about web development ! As a full-stack developer and a French speaker, I have a deep\n" +
+    "appreciation for contributing to open-source projects, and I'm constantly seeking new challenges to tackle.",
+  icons: {
+    icon: 'images/logo.png',
+    apple: 'images/logo.png',
+  },
+};
 
 export default function HomePage() {
   return (
