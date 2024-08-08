4306646c4d5938cd0fdac9f50a61355e5c1de358
CruuzAzul <alves.mckl@gmail.com>
2024-08-08 08:59:52 +0200

    🧠 Update header

--- modules/header/Header.tsx
+++ modules/header/Header.tsx
@@ -5,19 +5,31 @@ import styles from './Header.module.scss';
 import {IconLink} from '../navigation/links/IconLink';
 import {Navbar} from '../navigation/Navbar';
 import {MobileNavigation} from '../navigation/mobile/MobileNavigation';
-import {useTheme} from 'next-themes';
-import LogoLight from '../../public/images/logo.png';
-import LogoDark from '../../public/images/logoDark.png';
+import Picture from '../../public/images/mickael-alves.png';
 import Image from 'next/image';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import classNames from 'classnames';
 
 export const Header = () => {
-  const {theme} = useTheme();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <header className={styles.header}>
+    <header className={classNames(styles.header, { [styles.scrolled]: isScrolled })}>
       <Link href="/">
-        <Image src={theme === 'light' ? LogoLight : LogoDark} alt="logo" height={30} />
+        <Image className={styles.picture} src={Picture} alt="picture" height={50} />
       </Link>
 
       <MobileNavigation />
