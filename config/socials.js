38dabfd8a4d92cca508b087692b96c178609a9af
CruuzAzul <alves.mckl@gmail.com>
2025-01-04 12:51:58 +0100

    💳 Update card list

--- config/socials.js
+++ config/socials.js
@@ -4,26 +4,26 @@ export const socials = [
   {
     name: 'Bluesky',
     url: 'https://bsky.app/profile/cruuzazul.dev',
-    icon: <FaBluesky />,
-  },
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/CruuzAzul',
-    icon: <FaXTwitter />,
+    icon: <FaBluesky style={{ color: "#1285FE" }} />,
   },
   {
     name: 'GitHub',
     url: 'https://github.com/CruuzAzul',
-    icon: <FaGithub />,
+    icon: <FaGithub style={{ color: "#1F2329" }} />,
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/CruuzAzul',
+    icon: <FaXTwitter style={{ color: "#0F141A" }} />,
   },
   {
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/in/mickaelalves',
-    icon: <FaLinkedin />,
+    icon: <FaLinkedin style={{ color: "#0B66C2" }} />,
   },
   {
     name: 'Email',
     url: 'mailto:alves.mckl@gmail.com',
-    icon: <FaEnvelope />,
+    icon: <FaEnvelope style={{ color: "#38A5B1" }} />,
   },
 ];
